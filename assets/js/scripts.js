a8dd54e6bf3f92e4819f799b5ae900b88164040b
Seb Lee <sebastian.ma.lee@gmail.com>
2021-02-25 11:20:47 +0000

    Added audio playback to translated output

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,6 +7,7 @@ import {
 
 const inputArea = document.getElementById("input-area");
 const outputArea = document.getElementById("output-area");
+const audioButton = document.getElementById("audio-button");
 
 inputArea.addEventListener("keyup", (event) => {
   let textToTranslate = event.target.value;
@@ -18,6 +19,27 @@ inputArea.addEventListener("keyup", (event) => {
   } else {
     outputArea.innerHTML = formatOutput(
       translateEnglishToMorse(textToTranslate, translationKey)
+    );
+  }
+});
+
+audioButton.addEventListener("click", () => {
+  if (/^[.\/ -]*$/gm.test(outputArea.innerText)) {
+    const soundsToPlay = outputArea.innerText.split("");
+    let index = 0;
+    setInterval(() => {
+      if (soundsToPlay[index] === ".") {
+        new Audio("./assets/audio/dit.wav").play();
+      }
+
+      if (soundsToPlay[index] === "-") {
+        new Audio("./assets/audio/dah.wav").play();
+      }
+      index++;
+    }, 200);
+  } else {
+    window.speechSynthesis.speak(
+      new SpeechSynthesisUtterance(outputArea.innerText)
     );
   }
 });
